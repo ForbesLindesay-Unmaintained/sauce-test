b7ea34c06dc17643d0bf0aa29b7352033e10a1e6
Forbes Lindesay <forbes@lindesay.co.uk>
2016-12-30 13:06:52 +0000

    Add support for running tests at a location (without publishing an entry point)

--- index.js
+++ index.js
@@ -9,6 +9,82 @@ var runBrowsers = require('./lib/run-browsers');
 
 module.exports = runTests;
 module.exports.publishCode = publishCode;
+module.exports.runTestsAtLocation = runTestsAtLocation;
+function runTestsAtLocation(location, remote, options) {
+  addDefaultLoggingAndName(options);
+  if (remote === 'chromedriver') {
+    return runChromedriver(location, remote, {
+      name: options.name,
+      throttle: options.throttle,
+      platform: options.platform,
+      capabilities: options.capabilities,
+      debug: options.debug,
+      allowExceptions: options.allowExceptions,
+      testComplete: options.testComplete,
+      testPassed: options.testPassed,
+      timeout: options.timeout,
+      chromedriverStarted: true,
+      keepChromedriverAlive: false
+    }).then(function (result) {
+      options.onAllResults(result);
+      return result;
+    });
+  } else if (remote.indexOf('saucelabs') !== -1) {
+    return runSauceLabs(location, remote, {
+      name: options.name,
+      username: options.username,
+      accessKey: options.accessKey,
+      filterPlatforms: options.filterPlatforms,
+      choosePlatforms: options.choosePlatforms,
+      parallel: options.parallel,
+      platforms: options.platforms,
+      throttle: options.throttle,
+      capabilities: options.capabilities,
+      debug: options.debug,
+      jobInfo: options.jobInfo,
+      allowExceptions: options.allowExceptions,
+      testComplete: options.testComplete,
+      testPassed: options.testPassed,
+      bail: options.bail,
+      timeout: options.timeout,
+      onStart: options.onStart,
+      onQueue: options.onQueue,
+      onResult: options.onResult,
+      onBrowserResults: options.onBrowserResults
+    }).then(function (results) {
+      if (results.passedBrowsers && results.failedBrowsers) {
+        results.passed = results.failedBrowsers.length === 0;
+      }
+      options.onAllResults(results);
+      return results;
+    });
+  } else {
+    return runBrowsers(location, remote, {
+      name: options.name,
+      parallel: options.parallel,
+      platforms: options.platforms,
+      platform: options.platform,
+      throttle: options.throttle,
+      capabilities: options.capabilities,
+      debug: options.debug,
+      allowExceptions: options.allowExceptions,
+      testComplete: options.testComplete,
+      testPassed: options.testPassed,
+      bail: options.bail,
+      timeout: options.timeout,
+      onStart: options.onStart,
+      onQueue: options.onQueue,
+      onResult: options.onResult,
+      onBrowserResults: options.onBrowserResults
+    }).then(function (results) {
+      if (results.passedBrowsers && results.failedBrowsers) {
+        results.passed = results.failedBrowsers.length === 0;
+      }
+      options.onAllResults(results);
+      return results;
+    });
+  }
+}
 function runTests(entries, remote, options) {
   if (remote === 'chromedriver') {
     chromedriver.start();
@@ -22,79 +98,8 @@ function runTests(entries, remote, options) {
     stylesheets: options.stylesheets,
     html: options.html,
     disableSSL: options.disableSSL
-  }).then(function (location) {
+  }).then(location => {
     options.onPublish(location.url);
-    if (remote === 'chromedriver') {
-      return runChromedriver(location, remote, {
-        name: options.name,
-        throttle: options.throttle,
-        platform: options.platform,
-        capabilities: options.capabilities,
-        debug: options.debug,
-        allowExceptions: options.allowExceptions,
-        testComplete: options.testComplete,
-        testPassed: options.testPassed,
-        timeout: options.timeout,
-        chromedriverStarted: true,
-        keepChromedriverAlive: false
-      }).then(function (result) {
-        options.onAllResults(result);
-        return result;
-      });
-    } else if (remote.indexOf('saucelabs') !== -1) {
-      return runSauceLabs(location, remote, {
-        name: options.name,
-        username: options.username,
-        accessKey: options.accessKey,
-        filterPlatforms: options.filterPlatforms,
-        choosePlatforms: options.choosePlatforms,
-        parallel: options.parallel,
-        platforms: options.platforms,
-        throttle: options.throttle,
-        capabilities: options.capabilities,
-        debug: options.debug,
-        jobInfo: options.jobInfo,
-        allowExceptions: options.allowExceptions,
-        testComplete: options.testComplete,
-        testPassed: options.testPassed,
-        bail: options.bail,
-        timeout: options.timeout,
-        onStart: options.onStart,
-        onQueue: options.onQueue,
-        onResult: options.onResult,
-        onBrowserResults: options.onBrowserResults
-      }).then(function (results) {
-        if (results.passedBrowsers && results.failedBrowsers) {
-          results.passed = results.failedBrowsers.length === 0;
-        }
-        options.onAllResults(results);
-        return results;
-      });
-    } else {
-      return runBrowsers(location, remote, {
-        name: options.name,
-        parallel: options.parallel,
-        platforms: options.platforms,
-        platform: options.platform,
-        throttle: options.throttle,
-        capabilities: options.capabilities,
-        debug: options.debug,
-        allowExceptions: options.allowExceptions,
-        testComplete: options.testComplete,
-        testPassed: options.testPassed,
-        bail: options.bail,
-        timeout: options.timeout,
-        onStart: options.onStart,
-        onQueue: options.onQueue,
-        onResult: options.onResult,
-        onBrowserResults: options.onBrowserResults
-      }).then(function (results) {
-        if (results.passedBrowsers && results.failedBrowsers) {
-          results.passed = results.failedBrowsers.length === 0;
-        }
-        options.onAllResults(results);
-        return results;
-      });
-    }
+    return runTestsAtLocation(location, remote, options);
   });
 }
